f0de738e46c5a9ce1d1493389245d1805853d1da
Hanif Arafi <52808639+hanifarafi@users.noreply.github.com>
2020-10-05 19:12:51 +0700

    File Project Baru
    Penambahan berberapa file latihan dan pertemuan 6

---
+++ Pertemuan3/latihan2.php
@@ -0,0 +1,70 @@
+<?php
+
+//$nama = "Kirigaya Kazuto";   // String
+//$nama_panggilan = "Kirito";     // String
+//$umur = 17;     //
+//$jenis_kelamin = 'L';   // Char
+//$menikah = false;   // boolean
+//$tinggi_badan = 164.5;  // float
+//
+//$nama2 = "Yuuki Asuna";
+//$nama_panggilan2 = "Asuna";
+//$umur2 = 18;
+//$jenis_kelamin2 = 'P';
+//$menikah2 = false;
+//$tinggi_badan2 = 164.5;
+//
+// //$nama3 = "Ken Kaneki";
+// //$nama_panggilan3 = "Kaneki";
+// //$umur3 = 17;
+
+$nama = "Adam";
+$nama_panggilan = "Arthur";
+
+// $umur_kirito = 17;
+// $umur_kaneki = 17;
+
+// // Mencoba IF - ELSE
+// if ($umur_kirito > 17):
+//     echo "Lebih dari 17 th";
+// elseif ($umur_kirito < 16): 
+//     echo "Kurang dari 17 th";
+// else:
+//     echo "Sama dengan 17 th\n";
+//     if ($menikah === true): 
+//         echo "Sudah menikah";
+//     else:
+//         echo "Belum menikah";
+//     endif;
+// endif;
+// $nama = "Ayanokouji";
+// $umur_kirito = 17; // Integer
+// $umur_ayanokouji = '17'; // String
+// if ($umur_kirito === $umur_ayanokouji) :
+//     echo "Wah umur nya sama";
+// endif;
+// if ($umur_kaneki > 17):
+//     echo "sjdfsjjs";
+// elseif ($umur_ ):
+//     echo "isjfsjf";
+// endif;
+
+// Looping
+// For
+
+// for ($i = 0; $i < 10; $i++) :
+//     echo "Halo, nama saya $nama biasa dipanggil $nama_panggilan <br>";
+// endfor;
+
+// while
+$nilai = 1;
+while ($nilai < 10) :
+    echo "Hello, World!<br>";
+    $nilai++;
+endwhile;
+
+// do - while
+
+// do
+//    echo "Hello, World!";
+// while ($nilai > 10);
